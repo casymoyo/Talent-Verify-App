e8ff4e1ff73dabfef01bc21d0f792d4445c1042a
casper moyo <cassymyo@gmail.com>
2024-06-20 16:10:32 +0200

    'users#1'

---
+++ talentVerify_backend/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    role_choices = [
+        ('Admin', 'Admin'),
+        ('Employee', 'Employee'),
+        ('Support', 'Support')
+    ]
+    role = models.CharField(max_length='20', choices=role_choices)
+    profile_image = models.ImageField(upload_to='Profile_images', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.username} {self.role}'
